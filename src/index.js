7378cb8782a40a834320d6b7748deba7a913abc9
unknown <sumitshah43621@gmail.com>
2024-06-20 11:55:00 +0530

    few changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello, How are you?</h1>
+    <>Hello, How are you?</>
+    <>I am good, What about you?</>
   </React.StrictMode>
 );
 
